7c251b41c40ea5e0f55d95feb55a710eb7ce5a79
AB8928 <AB8928@student.jamk.fi>
2025-07-30 15:49:37 +0100

    modified

--- js/loadPreviusLogs.js
+++ js/loadPreviusLogs.js
@@ -3,3 +3,16 @@ import { createMarker } from "./marker.js";
 
 // ladataan aikaisemmin localStorageen lisätyt paikkamerkinnät kartalle
 export function loadPreviousLogs(map) {
+    const storedLogs = JSON.parse(localStorage.getItem("travelLogs")) || [];
+
+    storedLogs.forEach(async log => {
+        const place = {
+            location: {
+                lat: log.position.lat,
+                lng: log.position.lng
+            }
+        }
+        const { marker, infoWindow } = await createMarker(map, place);
+        createDiaryContent(infoWindow, log.title, log.date, log.text, marker, map);
+    });
+}
